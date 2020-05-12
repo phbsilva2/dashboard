aa3884f2549a442faf463641b1c5ba7f1ae23074
Paulo Borges <phbsilva@gmail.com>
2020-05-12 12:44:04 -0300

    Alterada da view index de Function Based View para Class Based View

--- jif/urls.py
+++ jif/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from . import views
+
+from .views import (
+    IndexView,
+)
 
 urlpatterns = [
-    path('', views.index)
+    path('', IndexView.as_view(), name='index')
 ]
