412214a02681d6b4c2afc13df967f413cb1aebcc
Paulo Borges <phbsilva@gmail.com>
2020-06-03 20:57:44 -0300

    Customizada a tela inicial do sistema
    Foram acrestentados cards com quantitativo de modalidade, atletas e inscrições.

--- jif/views/index_views.py
+++ jif/views/index_views.py
@@ -1,7 +1,16 @@
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from jif.models import Modalidade, Atleta, Inscricao
 
 
 class IndexView(LoginRequiredMixin, TemplateView):
     login_url = '/accounts/login/'
     template_name = 'jif/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["qtd_modalidades"] = Modalidade.objects.count()
+        context["qtd_atletas"] = Atleta.objects.count()
+        context["qtd_inscricoes"] = Inscricao.objects.count()
+
+        return context
