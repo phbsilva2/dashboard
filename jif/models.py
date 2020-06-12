69894d392df87260a45dda6fc048f8438e366de3
Paulo Borges <phbsilva@gmail.com>
2020-06-12 16:45:03 -0300

    Alterado a Models para incluir as novas tabelas do sistema
    Novas tabelas:
    - Instituto
    - Campus
    - Edicao
    - Etapa
    - EdicaoCampus
    - Categoria
    - EdicaoCategoria
    - Modalidade
    - EdicaoModalidade
    - Prova
    - EdicaoModalidadeProva
    - Atleta
    - Inscricao

--- jif/models.py
+++ jif/models.py
@@ -2,19 +2,13 @@ from django.db import models
 from stdimage.models import StdImageField
 
 
-class UnidadeOrganizacional(models.Model):
-    # Unidade Organizacional é ...
-    #
-    # UC02 - Manter Unidades Organizacionais:
-    #     FA01 - Incluir Unidade Organizacional:
-    #         2. O sistema apresenta os campos para entrada dos dados:
-    #             - Nome (Obrigatório / Não se repete)
-
+class Instituto(models.Model):
     nome = models.CharField(max_length=100, unique=True)
+    sigla = models.CharField(max_length=10, unique=True)
 
     class Meta:
-        verbose_name = 'Unidade Organizacional'
-        verbose_name_plural = 'Unidades Organizacionais'
+        verbose_name = 'Instituto'
+        verbose_name_plural = 'Institutos'
 
     def __str__(self):
         return self.nome
@@ -26,22 +20,112 @@ class UnidadeOrganizacional(models.Model):
         return self._meta.verbose_name_plural
 
 
-class TipoModalidade(models.Model):
-    # Tipo de Modalidade é ...
-    #
-    # UC11 – Manter Tipos de Modalidades:
-    #     FA01 - Incluir Tipo de Modalidade:
-    #         2. O sistema apresenta os campos para entrada dos dados:
-    #             - Título (Obrigatório / Não se repete)
-
-    titulo = models.CharField(max_length=100, unique=True, verbose_name='Título')
+class Campus(models.Model):
+    nome = models.CharField(max_length=100, unique=True)
+    instituto = models.ForeignKey(Instituto, on_delete=models.CASCADE, verbose_name='Instituto')
 
     class Meta:
-        verbose_name = 'Tipo de Modalidade'
-        verbose_name_plural = 'Tipos de Modalidades'
+        verbose_name = 'Campus'
+        verbose_name_plural = 'Campi'
 
     def __str__(self):
-        return self.titulo
+        return self.nome
+
+    def get_verbose_name(self):
+        return self._meta.verbose_name
+
+    def get_verbose_name_plural(self):
+        return self._meta.verbose_name_plural
+
+
+class Edicao(models.Model):
+    nome = models.CharField(max_length=100, unique=True)
+    data_inicio_edicao = models.DateField('Data do Início da Edição', blank=False, null=False)
+    data_termino_edicao = models.DateField('Data do Término da Edição', blank=False, null=False)
+
+    class Meta:
+        verbose_name = 'Edição'
+        verbose_name_plural = 'Edições'
+
+    def __str__(self):
+        return self.nome
+
+    def get_verbose_name(self):
+        return self._meta.verbose_name
+
+    def get_verbose_name_plural(self):
+        return self._meta.verbose_name_plural
+
+
+class Etapa(models.Model):
+    nome = models.CharField(max_length=100, unique=True)
+    data_inicio_etapa = models.DateField('Data do Início da Etapa', blank=False, null=False)
+    data_termino_etapa = models.DateField('Data do Término da Etapa', blank=False, null=False)
+    data_inicio_inscricao = models.DateField('Data do Início das Inscrições', blank=False, null=False)
+    data_termino_inscricao = models.DateField('Data do Término das Inscrições', blank=False, null=False)
+
+    class Meta:
+        verbose_name = 'Etapa'
+        verbose_name_plural = 'Etapas'
+
+    def __str__(self):
+        return self.nome
+
+    def get_verbose_name(self):
+        return self._meta.verbose_name
+
+    def get_verbose_name_plural(self):
+        return self._meta.verbose_name_plural
+
+
+class EdicaoCampus(models.Model):
+    edicao = models.ForeignKey(Edicao, on_delete=models.CASCADE, verbose_name='Edição')
+    campus = models.ForeignKey(Campus, on_delete=models.CASCADE, verbose_name='Campus')
+
+    class Meta:
+        verbose_name = 'Campus da Edição'
+        verbose_name_plural = 'Campi da Edição'
+
+    def __str__(self):
+        return f"{self.pk}"
+
+    def get_verbose_name(self):
+        return self._meta.verbose_name
+
+    def get_verbose_name_plural(self):
+        return self._meta.verbose_name_plural
+
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=100, unique=True)
+    idade_minima = models.IntegerField('Idade Mínima', blank=True, null=True)
+    idade_maxima = models.IntegerField('Idade Máxima', blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'Categoria'
+        verbose_name_plural = 'Categorias'
+
+    def __str__(self):
+        return self.nome
+
+    def get_verbose_name(self):
+        return self._meta.verbose_name
+
+    def get_verbose_name_plural(self):
+        return self._meta.verbose_name_plural
+
+
+class EdicaoCategoria(models.Model):
+    edicao = models.ForeignKey(Edicao, on_delete=models.CASCADE, verbose_name='Edição')
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, verbose_name='Categoria')
+    regras = models.TextField('Regras', blank=True)
+
+    class Meta:
+        verbose_name = 'Categoria da Edição'
+        verbose_name_plural = 'Categorias da Edição'
+
+    def __str__(self):
+        return f"{self.pk}"
 
     def get_verbose_name(self):
         return self._meta.verbose_name
@@ -51,27 +135,8 @@ class TipoModalidade(models.Model):
 
 
 class Modalidade(models.Model):
-    # Modalidade é ...
-    #
-    # UC03 - Manter Modalidades
-    #     FA01 - Incluir Modalidade
-    #         2. O sistema apresenta os campos para entrada dos dados:
-    #             - Nome (Obrigatório / Não se repete)
-    #             - Sigla (Obrigatório / Não se repete)
-    #             - Tipo (Obrigatório) [IndividuaL/Coletivo]
-    #             - Tipo de Modalidade (Obrigatório)
-    #
-    # UC04 - Manter Atletas por Modalidade
-    #     FA01 - Incluir Atleta por Modalidade
-    #         2. O sistema apresenta os campos para entrada dos dados:
-    #             - Modalidade (Obrigatório / Não se repete)
-    #             - Lotação Permitida (Obrigatório / Não se repete)
-
     nome = models.CharField(max_length=100, unique=True)
-    sigla = models.CharField(max_length=10, unique=True)
     tipo = models.CharField(max_length=1, choices=[['I', 'Individual'], ['C', 'Coletivo']])
-    tipo_modalidade = models.ForeignKey(TipoModalidade, on_delete=models.CASCADE, verbose_name='Tipo de Modalidade')
-    lotacao_permitida = models.IntegerField('Lotação Permitida')
 
     class Meta:
         verbose_name = 'Modalidade'
@@ -87,29 +152,69 @@ class Modalidade(models.Model):
         return self._meta.verbose_name_plural
 
 
-class Atleta (models.Model):
-    # Atleta é ...
-    #
-    # UC01 - Manter Atletas
-    #     FA01 - Incluir Atleta
-    #         2. O sistema apresenta os campos para entrada dos dados:
-    #             - Nome (Obrigatório)
-    #             - Foto
-    #             - CPF (Obrigatório / Não se repete)
-    #             - RG (Obrigatório / Não se repete)
-    #             - Matrícula (Obrigatório / Não se repete)
-    #             - Gênero (Obrigatório)
-    #             - Masculino;
-    #             - Feminino.
-    #             - Data de Nascimento
-    #             - Tipo Sanguíneo
-    #             - Plano de Saúde
-    #             - Nº Carteira SUS
-    #             - Medicamento Controlado
-    #             - Alergia
+class EdicaoModalidade(models.Model):
+    edicao_categoria = models.ForeignKey(EdicaoCategoria, on_delete=models.CASCADE, verbose_name='Categoria')
+    modalidade = models.ForeignKey(Modalidade, on_delete=models.CASCADE, verbose_name='Modalidade')
+    genero = models.CharField('Gênero', max_length=1, choices=[['M', 'Masculino'], ['F', 'Femenino']])
+    limite_participantes_modalidade = models.IntegerField('Limite de Participantes na Modalidade', blank=False, null=False)
 
+    class Meta:
+        verbose_name = 'Modalidade da Edição'
+        verbose_name_plural = 'Modalidades da Edição'
+
+    def __str__(self):
+        return f"{self.pk}"
+
+    def get_verbose_name(self):
+        return self._meta.verbose_name
+
+    def get_verbose_name_plural(self):
+        return self._meta.verbose_name_plural
+
+
+class Prova(models.Model):
+    nome = models.CharField(max_length=100, unique=True)
+    modalidade = models.ForeignKey(Modalidade, on_delete=models.CASCADE, verbose_name='Modalidade')
+
+    class Meta:
+        verbose_name = 'Prova'
+        verbose_name_plural = 'Provas'
+
+    def __str__(self):
+        return self.nome
+
+    def get_verbose_name(self):
+        return self._meta.verbose_name
+
+    def get_verbose_name_plural(self):
+        return self._meta.verbose_name_plural
+
+
+class EdicaoModalidadeProva(models.Model):
+    edicao_modalidade = models.ForeignKey(EdicaoModalidade, on_delete=models.CASCADE, verbose_name='Modalidade')
+    prova = models.ForeignKey(Prova, on_delete=models.CASCADE, verbose_name='Prova')
+    limite_equipe_campus = models.IntegerField('Limite de Equipe por Campus', blank=False, null=False)
+    limite_participante_equipe = models.IntegerField('Limite de Participante por Equipe', blank=False, null=False)
+    limite_participante_individual = models.IntegerField('Limite de Participante Individual', blank=False, null=False)
+
+    class Meta:
+        verbose_name = 'Prova da Modalidade'
+        verbose_name_plural = 'Provas da Modalidade'
+
+    def __str__(self):
+        return f"{self.pk}"
+
+    def get_verbose_name(self):
+        return self._meta.verbose_name
+
+    def get_verbose_name_plural(self):
+        return self._meta.verbose_name_plural
+
+
+class Atleta(models.Model):
     nome = models.CharField(max_length=100)
-    foto = StdImageField(upload_to='atletas', variations={'thumb': (124, 124)}, blank=True)  # TODO Aperfeiçoar parâmetros: https://pypi.org/project/django-stdimage/
+    foto = StdImageField(upload_to='atletas', variations={'thumb': (124, 124)},
+                         blank=True)  # TODO Aperfeiçoar parâmetros: https://pypi.org/project/django-stdimage/
     cpf = models.CharField('CPF', max_length=11, unique=True)
     rg = models.CharField('RG', max_length=50, unique=True)
     matricula = models.CharField('Matrícula', max_length=20, unique=True)
@@ -136,18 +241,10 @@ class Atleta (models.Model):
 
 
 class Inscricao(models.Model):
-    # Inscrição é ...
-    #
-    # UC07 - Manter Inscrições
-    #     FA01 - Incluir Inscrição
-    #         2. O sistema apresenta os campos para entrada dos dados:
-    #             - Modalidade (Obrigatório);
-    #             - Atleta (Obrigatório);
-    #             - Unidade (Obrigatório)
-
-    modalidade = models.ForeignKey(Modalidade, on_delete=models.CASCADE, verbose_name='Modalidade')
     atleta = models.ForeignKey(Atleta, on_delete=models.CASCADE, verbose_name='Atleta')
-    unidade_organizacional = models.ForeignKey(UnidadeOrganizacional, on_delete=models.CASCADE, verbose_name='Unidade Organizacional')
+    campus = models.ForeignKey(Campus, on_delete=models.CASCADE, verbose_name='Campus')
+    etapa = models.ForeignKey(Etapa, on_delete=models.CASCADE, verbose_name='Etapa')
+    edicao_prova = models.ForeignKey(EdicaoModalidadeProva, on_delete=models.CASCADE, verbose_name='Prova')
 
     objects = models.Manager()
 
